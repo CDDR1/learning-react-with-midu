c3ff12d1639a752f7a1c3b10677923655d628ddf
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-01-27 23:16:34 -0500

    Finished follow suggestion component styles

--- projects/00-hello-world/src/components/FollowSuggestion.jsx
+++ projects/00-hello-world/src/components/FollowSuggestion.jsx
@@ -1,9 +1,18 @@
 import "./FollowSuggestion.css";
 
-const FollowSuggestion = () => {
+const FollowSuggestion = ({ userAvatar, userName, userNickname, isFollowing }) => {
   return (
-    <h3>something...</h3>
+    <div className="suggestion-body">
+      <div className="avatar-container">
+        <img src={userAvatar} alt={`Avatar of ${userName}`} className="avatar" />
+      </div>
+      <div className="user-info">
+        <h3 className="user-name">{userName}</h3>
+        <span className="user-nickname">{userNickname}</span>
+      </div>
+      <button className="suggestion-button">{isFollowing ? "Unfollow" : "Follow"}</button>
+    </div>
   );
-}
+};
 
 export default FollowSuggestion;
--- projects/00-hello-world/src/components/TwitterCard.jsx
+++ projects/00-hello-world/src/components/TwitterCard.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import './TwitterCard.css';
-import FollowSuggestion from './FollowSuggestion';
+import React from "react";
+import "./TwitterCard.css";
+import FollowSuggestion from "./FollowSuggestion";
+import { useState } from "react";
 
 const TwitterCard = () => {
+  const suggestions = [
+    {
+      avatar: "https://xsgames.co/randomusers/avatar.php?g=male",
+      name: "Carlos Duque",
+      nickname: "@cddr1",
+      following: false,
+    },
+    {
+      avatar: "https://xsgames.co/randomusers/avatar.php?g=female",
+      name: "Jane Doe",
+      nickname: "@j4n3d03",
+      following: true,
+    },
+    {
+      avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+      name: "John Doe",
+      nickname: "@therealjohnny",
+      following: false,
+    },
+  ];
+
+  const [userSuggestions, setUserSuggestions] = useState(suggestions);
+
   return (
-    <div className='twitterCard-container'>
-      <h2 className='twitterCard-title'>Who to follow</h2>
-      <div className='followSuggestions-container'>
-        <FollowSuggestion />
+    <div className="twitterCard-container">
+      <h2 className="twitterCard-title">Who to follow</h2>
+      <div className="followSuggestions-container">
+        {userSuggestions.map((user, index) => (
+          <FollowSuggestion key={index} userAvatar={user.avatar} userName={user.name} userNickname={user.nickname} isFollowing={user.following} />
+        ))}
       </div>
     </div>
   );
