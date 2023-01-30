df8ac94ec908038f7f1087f17c42c04d24af5bff
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-01-30 10:25:03 -0500

    Created toggleFollow function.

--- projects/00-hello-world/src/components/TwitterCard.jsx
+++ projects/00-hello-world/src/components/TwitterCard.jsx
@@ -6,18 +6,21 @@ import { useState } from "react";
 const TwitterCard = () => {
   const suggestions = [
     {
+      id: 277,
       avatar: "https://xsgames.co/randomusers/avatar.php?g=male",
       name: "Carlos Duque",
       nickname: "@cddr1",
       following: false,
     },
     {
+      id: 278,
       avatar: "https://xsgames.co/randomusers/avatar.php?g=female",
       name: "Jane Doe",
       nickname: "@j4n3d03",
       following: true,
     },
     {
+      id: 279,
       avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel",
       name: "John Doe",
       nickname: "@therealjohnny",
@@ -27,12 +30,23 @@ const TwitterCard = () => {
 
   const [userSuggestions, setUserSuggestions] = useState(suggestions);
 
+  const toggleFollow = (suggestionId) => {
+    const updatedSuggestions = userSuggestions.map(suggestion => {
+      if (suggestion.id === suggestionId) {
+        suggestion.following = !suggestion.following;
+      }
+      return suggestion;
+    })
+
+    setUserSuggestions(updatedSuggestions);
+  };
+
   return (
     <div className="twitterCard-container">
       <h2 className="twitterCard-title">Who to follow</h2>
       <div className="followSuggestions-container">
-        {userSuggestions.map((user, index) => (
-          <FollowSuggestion key={index} userAvatar={user.avatar} userName={user.name} userNickname={user.nickname} isFollowing={user.following} />
+        {userSuggestions.map((user) => (
+          <FollowSuggestion key={user.id} id={user.id} userAvatar={user.avatar} userName={user.name} userNickname={user.nickname} isFollowing={user.following} />
         ))}
       </div>
     </div>
